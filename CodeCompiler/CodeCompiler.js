09f5bb51cbf8a54fab0c08374f555e8e2d7acd40
Ivan Golemdzhiyski <jackdye500@gmail.com>
2022-06-15 07:44:35 +0100

    added changes to backend to include starting code

--- CodeCompiler/CodeCompiler.js
+++ CodeCompiler/CodeCompiler.js
@@ -7,27 +7,41 @@ const { getCourses } = require("../functions/database");
 
 class CodeCompiler {
   async CompileCode(data) {
+    let errorMessage = "";
     const { source_code, language_id, course_id } = data;
+
     const selectedRows = await getCourses(course_id);
-    console.log(selectedRows);
-    const inputData = {
-      language_id: language_id,
-      source_code: source_code,
-    };
+    const decodeSourceCode = Buffer.from(source_code, "base64").toString(
+      "utf-8"
+    );
 
-    const submissionToken = await createSubmission(inputData);
-    const submissionOutput = await getSubmission(submissionToken);
+    if (!decodeSourceCode.includes(selectedRows.javascript_required_code)) {
+      errorMessage = `The code must include starting code to correctly run tests.`;
+    }
 
-    return sendResponse(200, {
-      message: "Success",
-      data: {
-        tabName: "Sample Test Case 0",
-        input: ["10", "1 1 3 1 2 1 3 3 3"],
-        output: ["None"],
-        expectedOutput: ["4"],
-        debugOutput: submissionOutput.stdout,
-      },
-    });
+    if (!errorMessage) {
+      const inputData = {
+        language_id: language_id,
+        source_code: source_code,
+      };
+
+      const submissionToken = await createSubmission(inputData);
+      const submissionOutput = await getSubmission(submissionToken);
+      return sendResponse(200, {
+        message: "Success",
+        data: {
+          tabName: "Sample Test Case 0",
+          input: ["10", "1 1 3 1 2 1 3 3 3"],
+          output: ["None"],
+          expectedOutput: ["4"],
+          debugOutput: submissionOutput.stdout,
+        },
+      });
+    } else {
+      return sendResponse(400, {
+        message: errorMessage,
+      });
+    }
   }
 }
 
--- functions/database.js
+++ functions/database.js
@@ -2,9 +2,17 @@ const { database } = require("../helpers/get-db-client");
 
 const getCourses = async (course_id) => {
   const selectedRows = await database("courses")
-    .select("id", "title", "description", "solution", "video_id")
+    .select(
+      "id",
+      "title",
+      "description",
+      "solution",
+      "video_id",
+      "starting_code_javascript",
+      "javascript_required_code"
+    )
     .where({ id: course_id });
-  return selectedRows;
+  return selectedRows[0];
 };
 
 module.exports = {
