3865b6d76cb63b86c1e0a7f69af42c5f49784e38
Angel Angelov <axxellblaze@gmail.com>
2022-06-27 01:49:24 +0300

    Resolved conflicts

--- functions/database.js
+++ functions/database.js
@@ -7,6 +7,13 @@ const getCourses = async (course_id) => {
   return selectedRows;
 };
 
+const getTestsByCourseIdAndSubmission = async (course_id, submission) => {
+  const selectedRows = await database("test_cases")
+    .select("id", "input", "expected_output")
+    .where({ course_id: course_id, submission: submission });
+  return selectedRows;
+};
+
 //A query to the courses table of the database. Should return the selected columns for every courses.
 const getAllCoursesQuery = async () => {
   const courses = await database("courses")
@@ -17,4 +24,5 @@ const getAllCoursesQuery = async () => {
 module.exports = {
   getCourses,
   getAllCoursesQuery,
+  getTestsByCourseIdAndSubmission,
 };
