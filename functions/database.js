9497bd1c6f2103fe384013b223b8d13e9ad94916
Angel Angelov <axxellblaze@gmail.com>
2022-06-27 01:54:31 +0300

    Resolved conflicts

--- functions/database.js
+++ functions/database.js
@@ -2,9 +2,22 @@ const { database } = require("../helpers/get-db-client");
 
 const getCourses = async (course_id) => {
   const selectedRows = await database("courses")
-    .select("id", "title", "description", "solution", "video_id")
+    .select(
+      "id",
+      "title",
+      "description",
+      "solution",
+      "video_id",
+      "starting_code_javascript",
+      "javascript_required_code",
+      "starting_code_python",
+      "starting_code_csharp",
+      "python_required_code",
+      "csharp_required_code",
+      "python_input_code"
+    )
     .where({ id: course_id });
-  return selectedRows;
+  return selectedRows[0];
 };
 
 const getTestsByCourseIdAndSubmission = async (course_id, submission) => {
@@ -23,6 +36,6 @@ const getAllCoursesQuery = async () => {
 
 module.exports = {
   getCourses,
-  getAllCoursesQuery,
   getTestsByCourseIdAndSubmission,
+  getAllCoursesQuery,
 };
